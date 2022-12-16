e3d818afed154c9612011f0dc8b34b8530357918
Alec Graham <maaaachoalpha@gmail.com>
2022-12-16 11:43:12 -0800

    components and stuff

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,16 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+import { COLORS, NFTData } from '../constants';
 
 
 const Home = () => {
   return (
-    <Text> HOmew </Text>
+<SafeAreaView style={{flex: 1}}>
 
+
+</SafeAreaView>
 )
 }
 
